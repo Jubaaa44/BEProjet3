7cc8f673dbde9536f5840b658ceefa6df23c91d6
Juba <abbacijuba@gmail.com>
2024-10-28 08:37:34 +0100

    First Commit

---
+++ src/main/java/com/projet3/Projet3OcApplication.java
@@ -0,0 +1,13 @@
+package com.projet3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet3OcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet3OcApplication.class, args);
+	}
+
+}
